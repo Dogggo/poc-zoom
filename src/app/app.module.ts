d31432801524bc9d929e38159d290b7457a8afba
Lukasz Myszkowski <lukasz.myszkowski@valueadd.pl>
2023-02-28 13:22:20 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxImageZoomModule} from "ngx-image-zoom";
+import {RedZoomModule} from "ngx-red-zoom";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxImageZoomModule,
+    RedZoomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
